c11093d61f99650cab389096bca413c98b874d0c
JamjarbinxGame <60180552+JamjarbinxGame@users.noreply.github.com>
2023-08-03 16:24:51 +0200

    Chnaged to MySql

--- Mercadona/Program.cs
+++ Mercadona/Program.cs
@@ -14,7 +14,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+    //options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
